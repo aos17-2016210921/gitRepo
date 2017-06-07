7ecb6fefe051ec5258f9d3b84ed580dac8754505
aos17-2016210921 <zar16@mails.tsinghua.edu.cn>
2017-06-07 20:16:24 +0800

    leave the state change unsolved

--- mpi/main.c
+++ mpi/main.c
@@ -1,47 +1,72 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
 #include"mpi.h"
+#define false 0
+#define true 1
+#define MAX_PROC 12
+
+/*if all node return phase OK,then return a plus to another phase*/
+int for_all_node(int* signal,int length,int check){
+	bool flag=true;
+	for(int i=0;i!=length;i++){
+		if(signal[i]!=check){
+			flag=false;
+		}
+	}
+	if(flag==true){
+		return 1;
+	}else{
+		return 0;
+	}
+}
 int main(int argc,char* argv[]){
 	int rank,size;
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
 	char worker[100]="./worker";
 	int total=atoi(argv[1]);
-	printf("total:%d\n",total);
+	int procNum=total/size;
+	if(total%size>rank){
+		procNum++;
+	}
+	printf("total:%d,%d,%d\n",total,procNum,size);
 	MPI_Comm* intercomm;
 	intercomm=malloc(sizeof(MPI_Comm)*1);
 	for(int i=0;i!=1;i++)
-		MPI_Comm_spawn(worker,MPI_ARGV_NULL,total,
+		MPI_Comm_spawn(worker,MPI_ARGV_NULL,procNum,
 				MPI_INFO_NULL,0,MPI_COMM_SELF,&intercomm[i],
 				MPI_ERRCODES_IGNORE);
-	MPI_Request req[2];
-	MPI_Status sta[2];
-	int alive[2];
+	int* alive;
+	alive=malloc(sizeof(int)*procNum);
 	int t=10;
 	int state=0;
-	while(t--){
-		for(int i=0;i!=2;i++){
+	int flag=true;
+	while(flag){
+		for(int i=0;i!=procNum;i++){
 			alive[i]=-1;
 		}
-		MPI_Request req[2];
-		MPI_Status sta[2];
-		int index[2];
-		for(int i=0;i!=2;i++){
+		MPI_Request req[MAX_PROC];
+		MPI_Status sta[MAX_PROC];
+		int index[MAX_PROC];
+		for(int i=0;i!=procNum;i++){
 				MPI_Irecv(&alive[i],1,MPI_INT,i,0,intercomm[0],&req[i]);
 		}
-		MPI_Testall(2,req,index,sta);
-		usleep(300000);
-		state=1;
-		for(int i=0;i!=2;i++){
-		//	if(alive[i]==1){
-			MPI_Send(&state,1,MPI_INT,i,0,intercomm[0]);
-		//	}else{
-		//		printf("bad\n");	
-		//	}
+		MPI_Testall(procNum,req,index,sta);
+		usleep(50000);
+		flag=false;
+		state+=for_all_node(alive,procNum,state);//?
+		MPI_Request reqs[MAX_PROC];
+		MPI_Status stas[MAX_PROC];
+		for(int i=0;i!=procNum;i++){
+				MPI_Isend(&state,1,MPI_INT,i,0,intercomm[0],&reqs[i]);
 		}
-		usleep(300000);
+		int index2[MAX_PROC];
+		MPI_Testall(procNum,reqs,index2,stas);
+		usleep(50000);
 	/*heartbeat*/
-		printf("%d %d\n",alive[0],alive[1]);
+		printf("rank:%d alive:%d %d\n",rank,alive[0],alive[1]);
 	}
 	MPI_Finalize();
 	return 0;
--- mpi/worker.c
+++ mpi/worker.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"mpi.h"
+#define false 0
+#define true 1
 int main(int argc,char* argv[]){
 	printf("start successfully\n");
 	MPI_Init(&argc,&argv);
@@ -10,17 +12,37 @@ int main(int argc,char* argv[]){
 	MPI_Comm_rank(parent,&rank);
 	printf("rank:%d\n",rank);
 	int t=10;
-	int info=0;
-	int state=-1;
-	while(t--){
+	int info=1;
+	int stage=-1;
+	bool flag=true;
+	while(flag){
 		MPI_Status sta;
 		state=-1;
-		info=1;
 		MPI_Send(&info,1,MPI_INT,0,0,parent);
-		MPI_Recv(&state,1,MPI_INT,0,0,parent,&sta);
-		printf("state:%d\n",state);
+		MPI_Recv(&stage,1,MPI_INT,0,0,parent,&sta);
+		//printf("state:%d\n",state);
+		switch(stage){
+			case 0:
+				flag=false;
+				break;
+			case 1:
+				map(&info);
+				break;
+			case 2:
+				reduce(&info);
+				break;
+			default:
+				info=1;
+				break;
+		}
 	}
 	MPI_Finalize();
 	printf("ret\n");
 	return 0;
+}
+void map(int &info){
+	info=2;
+}
+void reduce(int &info){
+	info=3
 }
